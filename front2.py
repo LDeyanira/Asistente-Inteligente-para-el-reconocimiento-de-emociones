c0a3c1d0dc5b9db933e54d2b8eaa84bebd26a941
Deyanira Lopez <lopezsdeyanira@gmail.com>
2024-10-01 22:09:32 -0600

    update

--- front2.py
+++ front2.py
@@ -1,97 +1,40 @@
 import streamlit as st
 from gtts import gTTS
 import io
-import nltk
-import speech_recognition as sr
 from chat_base import predict_class, get_response, intents
-
-# Descarga el tokenizador de NLTK si no está disponible
-try:
-    nltk.download('punkt', quiet=True)
-except Exception as e:
-    st.error("Error al descargar 'punkt': {}".format(e))
-
 # Funciones
+#funcion  para hablar 
 def speak(text):
     tts = gTTS(text=text, lang='es')
     audio_file = io.BytesIO()
     tts.write_to_fp(audio_file)
     audio_file.seek(0)
     st.audio(audio_file, format='audio/mp3')
-
-def listen():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        st.write("Escuchando...")
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio, language="es-ES")
-            return text
-        except sr.UnknownValueError:
-            st.error("No entendí lo que dijiste")
-            return None
-        except sr.RequestError:
-            st.error("Error al conectarse al servicio de reconocimiento de voz")
-            return None
-
-# Función para tokenizar de forma segura
-def safe_word_tokenize(sentence):
-    try:
-        return nltk.word_tokenize(sentence)
-    except LookupError:
-        st.error("El recurso 'punkt' no está disponible. Por favor, verifica la instalación de NLTK.")
-        return []
-
-# Interfaz de Streamlit
-st.title("AI")
-
+# Lógica del chatbot
 if "messages" not in st.session_state:
     st.session_state.messages = []
 if "first_message" not in st.session_state:
     st.session_state.first_message = True
+if "user_avatar" not in st.session_state:
+    st.session_state.user_avatar = "https://cdn.icon-icons.com/icons2/3399/PNG/512/bot_icon_214984.png"  # URL de la imagen del usuario
 
-# Mostrar mensajes anteriores
 for message in st.session_state.messages:
-    if message["role"] == "Bot":
-        with st.chat_message("Bot"):
-            st.markdown(message["content"])
-    else:
-        with st.chat_message("user"):
-            st.markdown(message["content"])
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-# Primer mensaje del bot
 if st.session_state.first_message:
-    initial_message = "Te doy la bienvenida, estoy aquí para ayudarte."
+    initial_message = "Hola,selecciona la planeación que necesites."
     with st.chat_message("Bot"):
         st.markdown(initial_message)
     st.session_state.messages.append({"role": "Bot", "content": initial_message})
     st.session_state.first_message = False
     speak(initial_message)
 
-# Botón para hablar
-if st.button("Hablar"):
-    prompt = listen()
-    if prompt:
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
-        # Implementación del algoritmo de la IA
-        insts = predict_class(prompt)
-        res = get_response(insts, intents)
-
-        with st.chat_message("Bot"):
-            st.markdown(res)
-        st.session_state.messages.append({"role": "Bot", "content": res})
-        speak(res)
-
-# Opción para ingresar texto manualmente si no se usa la voz
-if prompt := st.chat_input("Estoy para ti, ¿qué necesitas?"):
+if prompt := st.chat_input("¿Cómo puedo ayudarte?"):
     with st.chat_message("user"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Implementación del algoritmo de la IA
     insts = predict_class(prompt)
     res = get_response(insts, intents)
 
